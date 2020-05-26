c994db07c80cb573ec690bc2edab02b70459edff
oleksandrmoskalenko <alex_wk@ukr.net>
2020-05-26 23:40:49 +0300

    rewrote modal-window, added review (real review, i mean)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const API_KEY = '4518bf88af653a219ed6bca3f18e5fca';
-const API_URL = 'https://api.themoviedb.org/3/movie/550?api_key=4518bf88af653a219ed6bca3f18e5fca';
+const API_KEY = '29bb47b7552ec502eb87cebfbc77f766';
+const API_URL = 'https://api.themoviedb.org/3';
 const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
 
 $(document).ready(function () {
@@ -25,33 +25,12 @@ $(document).ready(function () {
   })
 
   $(document).on('click', '.movie__img', function () {
-    openModal()
+    $('.window').removeClass('hide')
   })
 
   //functions
 
-  function openModal() {
-    $('.modal-window').remove()
-    $('.movies').append(getReviews(movie.id))
-    $('.modal-window')
-      .css('position', 'fixed')
-      .css('z-index', '99')
-      .css('top', '50vh')
-      .css('left', '50%')
-      .css('width', '300px')
-      .css('height', '300px')
-      .css('background-color', '#000')
-    $(document).on('click', '.reviews__close', function () {
-      closeModal()
-    })
-  }
-
-  function closeModal() {
-    $('.modal-window').remove()
-  }
-
-
-  function getMovie() {
+  async function getMovie() {
     let query = $('.search__field').val()
 
     $('body').addClass('loading')
@@ -59,9 +38,8 @@ $(document).ready(function () {
     if (query !== '') {
       $('.movie').remove()
 
-
       $.ajax({
-        url: `${API_URL} /search/movie`,
+        url: `${API_URL}/search/movie`,
         type: 'GET',
         dataType: 'json',
         data: {
@@ -75,6 +53,11 @@ $(document).ready(function () {
           res.results.forEach((movie) => {
             if (movie.poster_path != null)
               $('.movies').append(drawMovie(movie))
+            $(`.${movie.id}`).click(() => {
+              getReviews(movie.id)
+              $('.reviews')
+                .append(`<h1 class="movie__title">${movie.title}</h1>`)
+            })
           })
         }
         $('body').removeClass('loading')
@@ -84,39 +67,52 @@ $(document).ready(function () {
   }
 
   function drawMovie(movie) {
-    let movieDOM = `< div class="movie" >
-    <img class="movie__roll" src="../images/roll.png" alt="roll">
-      <img class="movie__img" src="${IMG_URL + movie.poster_path}" alt="movie-img">
-        <div class="movie__review"
+    let movieDOM = `<div class="movie ${movie.id}">
+                      <img class="movie__roll" src="../images/roll.png" alt="roll">
+                      <img class="movie__img" src="${IMG_URL + movie.poster_path}" alt="movie-img">
+                      <div class="movie__review"
                       <h2 class="movie__title">${movie.title}</h2>
-      <div class="movie__info">
-        <h3><b>Release date: </b>${movie.release_date}</h3>
-        <h3><b>Rating: </b>${movie.vote_average}</h3>
-        <p class="movie__synopsis">${movie.overview}...</p>
-      </div>
+                      <div class="movie__info">
+                        <h3><b>Release date: </b>${movie.release_date}</h3>
+                        <h3><b>Rating: </b>${movie.vote_average}</h3>
+                        <p class="movie__synopsis">${movie.overview}...</p>
                       </div>
-                    </ > `
+                      </div>
+                      <h2 class="movie__title movie__title--front">${movie.title}</h2>
+                    </div>`
     return movieDOM
   }
 
+  function drawReview(movie) {
+    if (movie.results.length !== 0)
+      movie.results.forEach(item => {
+        $('.reviews').append(`
+      <h1 class="reviews__title">${item.author}</h1>
+      <p class="reviews__text">${item.content}</p>
+      `)
+
+      })
+    else {
+      $('.reviews').html('')
+      $('.window').addClass('hide')
+      console.log('Hey-hey-hey, man! No reviews, I said. No means not a single. Go away. Try later. Much later')
+    }
+    console.log(movie)
+  }
+
+  $(".window__close").click(() => {
+    $('.window').addClass('hide')
+    $('.reviews').html('')
+  })
+
   function getReviews(id) {
     $.ajax({
-      url: `${API_URL} /movie/${id} `,
+      url: `${API_URL}/movie/${id}/reviews?api_key=${API_KEY}`,
       type: 'GET',
       dataType: 'json',
       data: {
         api_key: API_KEY
       }
-    }).then((res) => {
-      if (res.results.length === 0)
-        alert('No reviews found')
-      else {
-        res.results.forEach((movie) => {
-          if (movie.poster_path != null)
-            $('.modal-window').append(drawMovie(movie))
-        })
-      }
-      $('body').removeClass('loading')
-    })
+    }).then((res) => drawReview(res))
   }
 })
